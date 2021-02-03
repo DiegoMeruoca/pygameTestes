6fe3c224d0480ac13204d83bc163f0ce803b0a1f
Diego Meruoca <diegomeruoca@gmail.com>
2021-02-03 20:52:56 -0300

    Menu funcionando parcialmente, falta calibrar o click

--- Plataforma.py
+++ Plataforma.py
@@ -104,11 +104,15 @@ pygame.init()  # Inicializando o pygame
 clock = pygame.time.Clock()  # Cria uma variavel de controle de clock (Ciclos/tempo)
 
 # Configurações da janela
-monitor_size = [pygame.display.Info().current_w, pygame.display.Info().current_h - 65]  # tamanho do monitor menos 65 pixels (Pra barra de titulo)
+monitor_size = [pygame.display.Info().current_w, pygame.display.Info().current_h - 60]  # tamanho do monitor menos 60 pixels (Pra barra de titulo)
 janela_tamanho = monitor_size  # Cria uma variavel com o tamanho da janela em pixels
 pygame.display.set_caption("Meu jogo")  # Define o título do game que aparece na janela
 janela_game = pygame.display.set_mode(janela_tamanho, 0, 32)  # Exibindo a tela
-display = pygame.Surface((640, 360))  # Cria o Display do game, area visivel do jogo
+# display = pygame.Surface((1280, 720))
+# display = pygame.Surface((854, 480))
+display = pygame.Surface((640, 360))  # Cria o Display do game, area visivel do jogo 360 p
+# Display com tamanho padronizado que sera escalado para o tamanho do monitor
+# Aumentar essa resolução resulta em melhor qualidade da imagem, porem com o jogo muito travado
 
 # Importação da imagens
 personagem_sprite = pygame.image.load('imagens/personagens/player1/parado/0.png')  # Adiciona a imagem do player  que está na pasta imagens
@@ -131,6 +135,10 @@ ilha_perto = pygame.transform.scale(ilha_perto, (100, 100))
 
 ilha_longe = pygame.image.load('imagens/cenario/ObjCenarioLonge.png')
 ilha_longe = pygame.transform.scale(ilha_longe, (120, 120))
+
+info_menu = pygame.image.load('imagens/menu/info_menu.png')
+info_menu = pygame.transform.scale(info_menu, (300, 200))
+
 
 # Importar os sons
 som_pulo = pygame.mixer.Sound('sons/pulo.wav')
@@ -155,17 +163,10 @@ personagem_espelhado = False
 fullscreen = False
 personagem_altura = 40
 personagem_largura = 32
+menu = True
+tela1 = False
 personagem_colisao = pygame.Rect(320, 50, personagem_largura, personagem_altura)  # Cria o retangulo de colisaão do personagem,
 # com base na sua posição e tamanho
-'''objetos_cenario = [[0.25, [220, 20, 'ilha_fundo']],  # [% de Paralax, [X, Y, tipo]
-                   [0.25, [480, 60, 'ilha_fundo']],
-                   [0.25, [730, 30, 'ilha_fundo']],
-                   [0.5, [230, 40, 'ilha_perto']],
-                   [0.5, [450, 90, 'ilha_perto']],
-                   [0.5, [810, 120, 'ilha_perto']],
-                   [0.5, [950, 45, 'ilha_perto']],
-                   [0.5, [1170, 100, 'ilha_perto']]]'''
-
 objetos_cenario = [[0.25, [200, random.randrange(10, 80), 'ilha_fundo']],  # [% de Paralax, [X, Y, tipo]
                    [0.25, [480, random.randrange(60, 140), 'ilha_fundo']],
                    [0.25, [730, random.randrange(20, 100), 'ilha_fundo']],
@@ -184,113 +185,130 @@ database_animacoes = {
     'pulando': carregar_animacoes('imagens/personagens/player1/pulando/', [14, 21], personagem_largura, personagem_altura)}
 
 while True:  # Lop infinito do game
-    # display.fill((146, 244, 255))  # Cor do fundo do game - 'Céu'
 
-    rolagem_tela = controle_rolagem_tela()  # Cahama o controle de camera
+    if menu:
+        display.blit(fundo, (0, 0))  # Preenche o fundo da tela coma a imagem
+        display.blit(info_menu, [165, 75])
+        for evento in pygame.event.get():  # event loop
+            if evento.type == QUIT:  # Se o evento for QUIT (sair)
+                pygame.quit()  # Finaliza o game e fecha a tela
+                sys.exit()  # Fecha o programa
 
-    display.blit(fundo, (0, 0))  # Preenche o fundo da tela coma a imagem
+            if evento.type == KEYDOWN:
+                if evento.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
 
-    preencher_fundo_cenario()  # Chama a funçao que preenche os itens de fundo do cenário
+            if evento.type == MOUSEBUTTONDOWN:
+                menu = False
+                tela1 = True
 
-    # loop para prrencher o mapa percorrendo a lista bidimensional na variavel mapa_game (Populada com dados do txt)
-    y = 0
-    colisao_blocos = []
-    for linha in mapa_game:
-        x = 0
-        for celula in linha:
-            if celula == '1':  # Se a celula for 1
-                display.blit(bloco_terra, (x * tamanho_blocos - rolagem_tela[0], y * tamanho_blocos - rolagem_tela[1]))
-                # Desenha a terra, na posição subtraimos rolagem_tela, isso permite que o bloco se mova para esquerda
-                # caso o valor de rolagem_tela aumente, e para esquerda caso diminua, dando o efeito de rolagem de tela
-            if celula == '2':
-                display.blit(bloco_grama, (x * tamanho_blocos - rolagem_tela[0], y * tamanho_blocos - rolagem_tela[1]))
-            if celula != '0':  # Se a celula for diferente de 0, ou seja, é um solido
-                colisao_blocos.append(pygame.Rect(x * tamanho_blocos, y * tamanho_blocos,
-                                                  tamanho_blocos, tamanho_blocos))
-                # Adiciona um bloco solido no array de solidos com a posição atual do mapa e tamanho de bloco padrão
-            x += 1
-        y += 1
+    if tela1:
+        rolagem_tela = controle_rolagem_tela()  # Cahama o controle de camera
 
-    # Movimentação
-    personagem_movimentacao = [0, 0]
-    if andando_dir:
-        personagem_movimentacao[0] += 2
-    if andando_esq:
-        personagem_movimentacao[0] -= 2
+        display.blit(fundo, (0, 0))  # Preenche o fundo da tela coma a imagem
 
-    personagem_movimentacao[1] += personagem_y_momentum  # Simular a gravidade
-    personagem_y_momentum += 0.3  # Poder da gravidade, qnd maior mais pesado, menor mais leve
-    if personagem_y_momentum > 5:  # Se a força da gravidade chegar a 5
-        personagem_y_momentum = 5  # Não aumentara mais, estaciona no 5
+        preencher_fundo_cenario()  # Chama a funçao que preenche os itens de fundo do cenário
 
-    # Alterar animação de acordo com o movimento
-    if not pulando:
-        if personagem_movimentacao[0] > 0:  # Se estiver se movendo para direita
-            animacao_atual, frame_aimacao_atual = alterar_animacao(animacao_atual, frame_aimacao_atual, 'correndo')
-            personagem_espelhado = False
-        if personagem_movimentacao[0] < 0:  # Se estiver se movendo para esquerda
-            animacao_atual, frame_aimacao_atual = alterar_animacao(animacao_atual, frame_aimacao_atual, 'correndo')
-            personagem_espelhado = True
-        if personagem_movimentacao[0] == 0:  # Se estiver se movendo para direita
-            animacao_atual, frame_aimacao_atual = alterar_animacao(animacao_atual, frame_aimacao_atual, 'parado')
-    else:
-        animacao_atual, frame_aimacao_atual = alterar_animacao(animacao_atual, frame_aimacao_atual, 'pulando')
-        if personagem_movimentacao[0] > 0:  # Se estiver se pulando para direita
-            personagem_espelhado = False
-        if personagem_movimentacao[0] < 0:  # Se estiver se pulando para esquerda
-            personagem_espelhado = True
+        # loop para prrencher o mapa percorrendo a lista bidimensional na variavel mapa_game (Populada com dados do txt)
+        y = 0
+        colisao_blocos = []
+        for linha in mapa_game:
+            x = 0
+            for celula in linha:
+                if celula == '1':  # Se a celula for 1
+                    display.blit(bloco_terra, (x * tamanho_blocos - rolagem_tela[0], y * tamanho_blocos - rolagem_tela[1]))
+                    # Desenha a terra, na posição subtraimos rolagem_tela, isso permite que o bloco se mova para esquerda
+                    # caso o valor de rolagem_tela aumente, e para esquerda caso diminua, dando o efeito de rolagem de tela
+                if celula == '2':
+                    display.blit(bloco_grama, (x * tamanho_blocos - rolagem_tela[0], y * tamanho_blocos - rolagem_tela[1]))
+                if celula != '0':  # Se a celula for diferente de 0, ou seja, é um solido
+                    colisao_blocos.append(pygame.Rect(x * tamanho_blocos, y * tamanho_blocos,
+                                                      tamanho_blocos, tamanho_blocos))
+                    # Adiciona um bloco solido no array de solidos com a posição atual do mapa e tamanho de bloco padrão
+                x += 1
+            y += 1
 
-    # Colisões
-    personagem_colisao, colisoes = mover(personagem_colisao, personagem_movimentacao, colisao_blocos)
+        # Movimentação
+        personagem_movimentacao = [0, 0]
+        if andando_dir:
+            personagem_movimentacao[0] += 2
+        if andando_esq:
+            personagem_movimentacao[0] -= 2
 
-    if colisoes['abaixo']:  # Se estiver no chão (Colidindo com algo abaixo)
-        personagem_y_momentum = 0
-        tempo_no_ar = 0
-        pulando = False
-    else:
-        tempo_no_ar += 1
+        personagem_movimentacao[1] += personagem_y_momentum  # Simular a gravidade
+        personagem_y_momentum += 0.3  # Poder da gravidade, qnd maior mais pesado, menor mais leve
+        if personagem_y_momentum > 5:  # Se a força da gravidade chegar a 5
+            personagem_y_momentum = 5  # Não aumentara mais, estaciona no 5
 
-    if colisoes['acima']:
-        personagem_y_momentum = 0
+        # Alterar animação de acordo com o movimento
+        if not pulando:
+            if personagem_movimentacao[0] > 0:  # Se estiver se movendo para direita
+                animacao_atual, frame_aimacao_atual = alterar_animacao(animacao_atual, frame_aimacao_atual, 'correndo')
+                personagem_espelhado = False
+            if personagem_movimentacao[0] < 0:  # Se estiver se movendo para esquerda
+                animacao_atual, frame_aimacao_atual = alterar_animacao(animacao_atual, frame_aimacao_atual, 'correndo')
+                personagem_espelhado = True
+            if personagem_movimentacao[0] == 0:  # Se estiver se movendo para direita
+                animacao_atual, frame_aimacao_atual = alterar_animacao(animacao_atual, frame_aimacao_atual, 'parado')
+        else:
+            animacao_atual, frame_aimacao_atual = alterar_animacao(animacao_atual, frame_aimacao_atual, 'pulando')
+            if personagem_movimentacao[0] > 0:  # Se estiver se pulando para direita
+                personagem_espelhado = False
+            if personagem_movimentacao[0] < 0:  # Se estiver se pulando para esquerda
+                personagem_espelhado = True
 
-    # Controle da animação
-    frame_aimacao_atual += 1
-    if frame_aimacao_atual >= len(database_animacoes[animacao_atual]):
-        frame_aimacao_atual = 0
-    personagem_sprite_id = database_animacoes[animacao_atual][frame_aimacao_atual]
-    personagem_sprite = frames_animacao[personagem_sprite_id]
-    # Renderiza a imagem do personagem sobre a tela na posição do personagem_colisao
-    display.blit(pygame.transform.flip(personagem_sprite, personagem_espelhado, False),
-                 (personagem_colisao.x - rolagem_tela[0], personagem_colisao.y - rolagem_tela[1]))
-    # Transform flip espelha ou não o personagem de acordo com a variavel personagem_espelhado
+        # Colisões
+        personagem_colisao, colisoes = mover(personagem_colisao, personagem_movimentacao, colisao_blocos)
 
-    # Captura de eventos
-    for evento in pygame.event.get():  # event loop
-        if evento.type == QUIT:  # Se o evento for QUIT (sair)
-            pygame.quit()  # Finaliza o game e fecha a tela
-            sys.exit()  # Fecha o programa
+        if colisoes['abaixo']:  # Se estiver no chão (Colidindo com algo abaixo)
+            personagem_y_momentum = 0
+            tempo_no_ar = 0
+            pulando = False
+        else:
+            tempo_no_ar += 1
 
-        if evento.type == KEYDOWN:
-            if evento.key == K_ESCAPE:
-                pygame.quit()
-                sys.exit()
+        if colisoes['acima']:
+            personagem_y_momentum = 0
 
-        if evento.type == KEYDOWN:  # Ao pressionar alguma tecla
-            if evento.key == K_RIGHT:  # Se for direita
-                andando_dir = True  # Andar pra direita passa a ser verdadeiro
-            if evento.key == K_LEFT:
-                andando_esq = True
-            if evento.key == K_SPACE:
-                if tempo_no_ar < 6:  # Para um double jump experimente 30
-                    personagem_y_momentum = -5
-                    pulando = True
-                    som_pulo.play()
+        # Controle da animação
+        frame_aimacao_atual += 1
+        if frame_aimacao_atual >= len(database_animacoes[animacao_atual]):
+            frame_aimacao_atual = 0
+        personagem_sprite_id = database_animacoes[animacao_atual][frame_aimacao_atual]
+        personagem_sprite = frames_animacao[personagem_sprite_id]
+        # Renderiza a imagem do personagem sobre a tela na posição do personagem_colisao
+        display.blit(pygame.transform.flip(personagem_sprite, personagem_espelhado, False),
+                     (personagem_colisao.x - rolagem_tela[0], personagem_colisao.y - rolagem_tela[1]))
+        # Transform flip espelha ou não o personagem de acordo com a variavel personagem_espelhado
 
-        if evento.type == KEYUP:  # Ao soltar alguma tecla
-            if evento.key == K_RIGHT:  # Se for seta direita
-                andando_dir = False  # Andar direita passa a ser falso
-            if evento.key == K_LEFT:
-                andando_esq = False
+        # Captura de eventos
+        for evento in pygame.event.get():  # event loop
+            if evento.type == QUIT:  # Se o evento for QUIT (sair)
+                pygame.quit()  # Finaliza o game e fecha a tela
+                sys.exit()  # Fecha o programa
+
+            if evento.type == KEYDOWN:
+                if evento.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+
+            if evento.type == KEYDOWN:  # Ao pressionar alguma tecla
+                if evento.key == K_RIGHT:  # Se for direita
+                    andando_dir = True  # Andar pra direita passa a ser verdadeiro
+                if evento.key == K_LEFT:
+                    andando_esq = True
+                if evento.key == K_SPACE:
+                    if tempo_no_ar < 6:  # Para um double jump experimente 30
+                        personagem_y_momentum = -5
+                        pulando = True
+                        som_pulo.play()
+
+            if evento.type == KEYUP:  # Ao soltar alguma tecla
+                if evento.key == K_RIGHT:  # Se for seta direita
+                    andando_dir = False  # Andar direita passa a ser falso
+                if evento.key == K_LEFT:
+                    andando_esq = False
 
     # Escala o display (onde estão os elementos do jogo) para o tamanho da janela
     tela_visivel = pygame.transform.scale(display, janela_tamanho)
